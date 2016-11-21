8f270c2ca6334563ae61e207ea109a0a971586a8
Ben Fleming <benfleming121@gmail.com>
2016-11-21 21:39:13 +1300

    File Upload

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+  <title>Ben The Ninja | Home</title>
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+  <link rel="shortcut icon" href="img/favicon.ico"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+  <div class="wrapper">
+    <div class="text">
+      <h1>Ben Fleming</h1>
+      <h3>Web Design and Development.</h3>
+      <h4>Location: New Zealand</h4>
+      <h4>Email: benfleming121@gmail.com</h4>
+    </div>
+  </div>
+</body>
+</html>
